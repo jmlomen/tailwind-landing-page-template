e62fa81f63829d32aebf20bcda0550a7e971ee01
jmlomen <john.lomen@gmail.com>
2026-08-20 16:45:29 +1000

    chore: try different navbar (#1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/navbar";
 import "./css/style.css";
 
 import { Inter } from "next/font/google";
@@ -24,6 +25,7 @@ export default function RootLayout({
         className={`${inter.variable} bg-gray-50 font-inter tracking-tight text-gray-900 antialiased`}
       >
         <div className="flex min-h-screen flex-col overflow-hidden supports-[overflow:clip]:overflow-clip">
+          <Navbar />
           {children}
         </div>
       </body>
