d9b11c9ca25ac54686fc93ef981ff94cb61160db
jmlomen <john.lomen@gmail.com>
2026-08-22 19:58:56 +1000

    GitHub terraform workflow (#4)
    * chore: add github workflow for terraform
    
    * chore: make add s3 to test gh workflow for terraform

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,7 @@
 resource "aws_s3_bucket" "sandbox_test" {
   bucket = "sandbox-terraform-test-715831355634"
 }
+
+resource "aws_s3_bucket" "sandbox_deploy_test" {
+  bucket = "sandbox-terraform-deploy-test-715831355634"
+}
