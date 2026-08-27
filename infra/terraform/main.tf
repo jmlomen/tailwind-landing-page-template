8fddf4719f5aa6211a7057d55773b1634b0f17ed
Johnny <john.lomen@gmail.com>
2026-08-27 20:02:12 +1000

    chore: cleanup code moved to infra-bootstrap

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,3 +2,12 @@ resource "aws_s3_bucket" "sandbox_test" {
   bucket = "sandbox-terraform-test-715831355634"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-jmlomen-main"
+    key            = "tailwind-landing-page-template/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+  }
+}
+
